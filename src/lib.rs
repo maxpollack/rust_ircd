0bb73ee8ff4ce95b3454e2cbc834437ec7e5f398
Max Pollack <max@wh1sk3y.com>
2019-12-25 12:58:47 -0500

    Handle incoming connections and echo the sent commands.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,50 @@
-use clap::ArgMatches;
+use clap::{value_t, ArgMatches};
 use log::*;
 use pretty_env_logger;
+use std::io;
+use std::net::TcpListener;
+use std::process;
+
+mod client;
 
 pub fn run(config: ArgMatches) {
     pretty_env_logger::init();
 
-    let port = config.value_of("port").unwrap();
+    debug!("Initializing rust_irc...");
 
-    debug!("Loading rust_irc...");
+    let port = value_t!(config.value_of("port"), u32);
+    let port = port.unwrap_or_else(|_| {
+        error!("Failed to parse port! Port value must be a valid number!");
+        process::exit(1);
+    });
+
+    if let Err(e) = serve(port) {
+        error!("Failed to bind to address! {}", e);
+    }
+}
+
+fn serve(port: u32) -> io::Result<()> {
     debug!("Binding to port {}", port);
+
+    let bind_address = format!("127.0.0.1:{}", port);
+    let listener = TcpListener::bind(bind_address)?;
+    let mut clients: Vec<client::Client> = Vec::new();
+
+    debug!("Successfully bound to port {}", port);
+
+    for mut stream in listener.incoming() {
+        if let Ok(stream) = stream {
+            let client = client::Client::new(stream);
+
+            if let Ok(client) = client {
+                clients.push(client);
+            } else {
+                error!("Failed to initialize client for incoming connection.");
+            }
+        } else {
+            error!("Error retreiving incoming connection stream!");
+        }
+    }
+
+    Ok(())
 }
