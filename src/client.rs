81eb4d76c563248c54baf611c242ba9aba829652
Max Pollack <max@leagueside.com>
2019-12-25 15:39:34 -0500

    Added stub of command parser, mpsc channel to pass commands from clients to server.

--- src/client.rs
+++ src/client.rs
@@ -2,10 +2,16 @@ use log::*;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::net::TcpStream;
+use std::sync::mpsc::Sender;
+use std::sync::Arc;
 use std::thread;
 
+mod command_parser;
+
+pub use command_parser::ClientCommand;
+
 pub struct Client {
-    join_handle: thread::JoinHandle<()>,
+    join_handle: Option<thread::JoinHandle<()>>,
 }
 
 pub enum ClientError {
@@ -13,7 +19,7 @@ pub enum ClientError {
 }
 
 impl Client {
-    pub fn new(stream: TcpStream) -> Result<Client, ClientError> {
+    pub fn new(stream: TcpStream, sender: Sender<ClientCommand>) -> Result<Client, ClientError> {
         let address = stream.peer_addr();
 
         let address = match address {
@@ -25,34 +31,66 @@ impl Client {
 
         debug!("Accepted new connection from {}", address.ip());
 
-        let mut reader = BufReader::new(stream);
+        let mut client = Client { join_handle: None };
 
+        client.start(stream, address, sender);
+
+        Ok(client)
+    }
+
+    fn start(
+        &mut self,
+        stream: TcpStream,
+        address: std::net::SocketAddr,
+        sender: Sender<ClientCommand>,
+    ) {
         let join_handle = thread::spawn(move || {
+            let mut reader = BufReader::new(stream);
             loop {
-                let mut command = String::new();
-                let read_result = reader.read_line(&mut command);
-
-                match read_result {
-                    Ok(length) => {
-                        // 0 length read means a dead socket.
-                        if length == 0 {
-                            break;
-                        }
-
-                        debug!("Read command: {}", command.trim());
+                match command_from_reader(&mut reader) {
+                    CommandResult::Disconnected => {
+                        debug!(
+                            "Socket disconnect detected from {}, killing thread.",
+                            address.ip()
+                        );
+                        break;
                     }
-                    Err(e) => {
-                        error!("Error on read! {}", e);
+                    CommandResult::Received(command) => {
+                        debug!("{} sent text {}", address, command.trim());
+
+                        let command = ClientCommand::parse(&command);
+
+                        sender.send(command).unwrap();
                     }
                 }
             }
-
-            debug!(
-                "Socket disconnect detected from {}, killing thread.",
-                address.ip()
-            );
         });
 
-        Ok(Client { join_handle })
+        self.join_handle = Some(join_handle);
+    }
+}
+
+enum CommandResult {
+    Received(String),
+    Disconnected,
+}
+
+fn command_from_reader(reader: &mut BufReader<TcpStream>) -> CommandResult {
+    let mut command = String::new();
+    let read_result = reader.read_line(&mut command);
+
+    match read_result {
+        Ok(length) => {
+            // 0 length read means a dead socket.
+            if length == 0 {
+                return CommandResult::Disconnected;
+            }
+
+            CommandResult::Received(command)
+        }
+        Err(e) => {
+            error!("Error on read! {}", e);
+            CommandResult::Disconnected
+        }
     }
 }
